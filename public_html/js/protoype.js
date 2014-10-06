deca881a5c3f0ea7e102a4c441f753328e476811
Orde Saunders <orde@decadecity.net>
2014-10-06 11:44:27 +0100

    Basic flow for favorite + date.

--- public_html/js/protoype.js
+++ public_html/js/protoype.js
@@ -1,17 +1,65 @@
 $(document).ready(function() {
+  $('[data-ct-form-default]').each(function() {
+    var type = $(this).data('ct-form-default');
+    if (type === 'today') {
+      //this.value = new Date();
+    }
+  });
+});
+
+$(document).ready(function() {
+  var advanceStage = function() {
+    stage += 1;
+    $('[data-ct-ui-stage]').addClass('ui--removed');
+    $('[data-ct-ui-stage=' + stage + ']').removeClass('ui--removed');
+  };
+
+  var stage = 1;
+  if (window.location.hash) {
+    var new_stage = parseInt(window.location.hash.slice(1), 10);
+    if (!isNaN(new_stage)) {
+      stage = new_stage - 1;
+      advanceStage();
+    }
+  }
+
   $('[data-ct-ui-show-target]').on('click', function() {
     var target = $(this).data('ct-ui-show-target');
-    $('[data-ct-ui-show-' + target + ']').removeClass('ui--hidden');
-    $('[data-ct-ui-hide-' + target + ']').addClass('ui--hidden');
+    $('[data-ct-ui-show-' + target + ']').removeClass('ui--removed');
+    $('[data-ct-ui-hide-' + target + ']').addClass('ui--removed');
   });
+
   $('[data-ct-ui-show-checked]').on('change', function() {
     var target = $(this).data('ct-ui-show-checked');
     if (this.value === 'on') {
-      $('[data-ct-ui-checked-show-' + target + ']').removeClass('ui--hidden');
-      $('[data-ct-ui-checked-hide-' + target + ']').addClass('ui--hidden');
+      $('[data-ct-ui-checked-show-' + target + ']').removeClass('ui--removed');
+      $('[data-ct-ui-checked-hide-' + target + ']').addClass('ui--removed');
     } else {
-      $('[data-ct-ui-checked-show-' + target + ']').addClass('ui--hidden');
-      $('[data-ct-ui-checked-hide-' + target + ']').removeClass('ui--hidden');
+      $('[data-ct-ui-checked-show-' + target + ']').addClass('ui--removed');
+      $('[data-ct-ui-checked-hide-' + target + ']').removeClass('ui--removed');
     }
   });
+
+  $('[data-ct-ui-stage-change=click]').on('click', function() {
+    advanceStage();
+  });
+  $('[data-ct-ui-stage-change=change]').on('change', function() {
+    advanceStage();
+  });
+  $('[data-ct-ui-stage-change=checked]').on('change', function() {
+    if (this.value === 'on') {
+      advanceStage()
+    }
+  });
+  $('[data-ct-ui-stage-change=complete]').on('blur', function() {
+    if (this.value && this.value !== 'off') {
+      advanceStage()
+    }
+  });
+
+  $('[data-ct-ui-restart]').on('click', function() {
+    stage = 0;
+    advanceStage();
+  });
+
 });
