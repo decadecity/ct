f93d60f582019c34c16031701efbe4ad1b49ab36
Orde Saunders <orde@decadecity.net>
2014-12-26 12:30:15 +0000

    Removing debug.

--- caffeine_tracker/apps/data/templatetags/data_extras.py
+++ caffeine_tracker/apps/data/templatetags/data_extras.py
@@ -4,10 +4,7 @@ from caffeine_tracker.apps.data.utils import current_caffeine
 
 register = template.Library()
 
-from pprint import pprint
-
 @register.simple_tag(takes_context=True)
 def users_current_caffeine(context):
-    pprint(context)
     user = context['user']
     return current_caffeine(user)
